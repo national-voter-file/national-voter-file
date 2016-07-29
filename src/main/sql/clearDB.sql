30bd5cc8082385c8b5b971e17b04755b14d3c102
Ben <ben@peartreestudio.net>
2016-07-29 13:49:59 -0500

    Fixed issues after attempting some big loads

--- src/main/sql/clearDB.sql
+++ src/main/sql/clearDB.sql
@@ -2,8 +2,8 @@
 -- WARNING this script will delete all of your data!
 
 truncate table VOTER_REPORT_FACT;
-truncate table MAILING_ADDRESS_DIM;
-truncate table HOUSEHOLD_DIM;
+truncate table MAILING_ADDRESS_DIM CASCADE;
+truncate table HOUSEHOLD_DIM CASCADE;
 truncate table PERSON_DIM CASCADE;
-truncate table VOTER_DIM;
+truncate table VOTER_DIM CASCADE;
 
