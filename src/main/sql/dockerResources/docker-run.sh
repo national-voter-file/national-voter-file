5ef0861bf5af4ff1347c79d78af27fb5f33f7ea9
Ben <ben@peartreestudio.net>
2016-09-06 22:43:19 -0500

    Changed port for docker postgres back to 54321

--- src/main/sql/dockerResources/docker-run.sh
+++ src/main/sql/dockerResources/docker-run.sh
@@ -11,9 +11,9 @@ fi
 echo "attaching to container. Press ctrl-C to stop the service."
 docker attach nvf_postgis || \
 docker start nvf_postgis && docker attach nvf_postgis || \
-docker run --name nvf_postgis -p 5433:5432 -e POSTGRES_PASSWORD=$1 voterfile/postgis
+docker run --name nvf_postgis -p 54321:5432 -e POSTGRES_PASSWORD=$1 voterfile/postgis
 
 # Open up the ports to allow remote access to db
-iptables -A INPUT -p tcp -m tcp --sport 5433 -j ACCEPT
-iptables -A OUTPUT -p tcp -m tcp --sport 5433 -j ACCEPT
+iptables -A INPUT -p tcp -m tcp --sport 54321 -j ACCEPT
+iptables -A OUTPUT -p tcp -m tcp --sport 54321 -j ACCEPT
 
