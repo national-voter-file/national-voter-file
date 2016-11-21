c2f726c4e686d0da97a3e34c32972e6b049a93b3
JoeNVF <valsacar@gmail.com>
2016-11-21 21:24:54 +0100

    Typo, made file use relative address instead of fixed address

--- docker/buildCensus.sh
+++ docker/buildCensus.sh
@@ -3,21 +3,21 @@
 ################### 
 ## Populate the date dimension with 20 years worth
 ## of days
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /../src/main/pdi/populateDateDimension.ktr
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file ../src/main/pdi/populateDateDimension.ktr
 
 ################################
 ##### Load Electoral Jurisdiction Dimensions
 ################################
 
 ## State Assembly
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /../src/main/pdi/LoadLowerHouseDim.ktr -param:fileName=/../dimensionaldata/StateAssembly.txt 
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file ../src/main/pdi/LoadLowerHouseDim.ktr -param:fileName=/../dimensionaldata/StateAssembly.txt 
 
 ## State Sentate
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /../src/main/pdi/LoadUpperHouseDim.ktr -param:fileName=/../dimensionaldata/StateSenate.txt 
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file ../src/main/pdi/LoadUpperHouseDim.ktr -param:fileName=/../dimensionaldata/StateSenate.txt 
 
 ## Congressional Districts
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /../src/main/pdi/LoadCongressionalDistsDim.ktr -param:fileName=/../dimensionaldata/congress.csv 
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file ../src/main/pdi/LoadCongressionalDistsDim.ktr -param:fileName=/../dimensionaldata/congress.csv 
 
 ## Counties
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /../src/main/pdi/LoadCountyDim.ktr -param:fileName=/../dimensionaldata/counties.csv 
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file ../src/main/pdi/LoadCountyDim.ktr -param:fileName=../dimensionaldata/counties.csv 
 
