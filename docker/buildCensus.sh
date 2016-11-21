a4056c8006d10e1371c7f3a3700d37731a6f4f22
JoeNVF <valsacar@gmail.com>
2016-11-21 22:14:30 +0100

    Another typo fix

--- docker/buildCensus.sh
+++ docker/buildCensus.sh
@@ -10,13 +10,13 @@ docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-vote
 ################################
 
 ## State Assembly
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-voter-file/src/main/pdi/LoadLowerHouseDim.ktr -param:fileName=//national-voter-file/dimensionaldata/StateHouse.csv 
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-voter-file/src/main/pdi/LoadLowerHouseDim.ktr -param:fileName=//national-voter-file/dimensionaldata/stateHouse.csv 
 
 ## State Sentate
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-voter-file/src/main/pdi/LoadUpperHouseDim.ktr -param:fileName=//national-voter-file/dimensionaldata/StateSenate.csv 
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-voter-file/src/main/pdi/LoadUpperHouseDim.ktr -param:fileName=/national-voter-file/dimensionaldata/stateSenate.csv 
 
 ## Congressional Districts
-docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-voter-file/src/main/pdi/LoadCongressionalDistsDim.ktr -param:fileName=//national-voter-file/dimensionaldata/congress.csv 
+docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-voter-file/src/main/pdi/LoadCongressionalDistsDim.ktr -param:fileName=/national-voter-file/dimensionaldata/congress.csv 
 
 ## Counties
 docker-compose run etl /opt/pentaho/data-integration/pan.sh -file /national-voter-file/src/main/pdi/LoadCountyDim.ktr -param:fileName=/national-voter-file/dimensionaldata/counties.csv 
