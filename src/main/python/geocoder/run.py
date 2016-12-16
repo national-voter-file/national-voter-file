f094bdadf67409333af40821df564fb60809503e
Tanya Schlusser <tanya@tickel.net>
2016-12-16 08:43:19 -0600

    Add geocoder skeleton, with config file and structure to allow 'daemonization'

---
+++ src/main/python/geocoder/run.py
@@ -0,0 +1,28 @@
+"""
+run
+~~~
+
+Empty main module; daemon.py will call `run.run(config)`, and
+`config` will contain the entire dictionary object as written in
+_daemon.conf_.
+"""
+import logging
+import time
+
+log = logging.getLogger()
+
+
+def run(config, sleep_duration=2.9):
+    while True:
+        log.info('Running the main module.')
+
+        db_config = config['databases']
+        log.info('Database configurations:\n{}'.format(db_config))
+
+        mapzen_config = config['mapzen']
+        log.info('Mapzen configuration:\n{}'.format(mapzen_config))
+
+        tamu_config = config['tamu']
+        log.info('Tamu configuration:\n{}'.format(tamu_config))
+
+        time.sleep(sleep_duration)
