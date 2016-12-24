6584d58d1ed1734ce6331173e0e00f694b5ac718
Ben <ben@peartreestudio.net>
2016-12-24 13:48:14 -0600

    Fixed up a couple of issues in jurisdictions

--- docker/postgis/dockerResources/z-init-db.sh
+++ docker/postgis/dockerResources/z-init-db.sh
@@ -3,4 +3,3 @@
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -a --dbname=VOTER < /sql/create_database.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -a --dbname=VOTER < /sql/create_tables.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -a --dbname=VOTER < /sql/populate_static_data.sql
-
