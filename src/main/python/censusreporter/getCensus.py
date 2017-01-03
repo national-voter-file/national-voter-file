481095bce62bed71486a4b1acaa86d9391098b2f
JoeNVF <valsacar@gmail.com>
2017-01-03 21:21:56 +0100

    90% tracts completed

--- src/main/python/censusreporter/getCensus.py
+++ src/main/python/censusreporter/getCensus.py
@@ -10,6 +10,13 @@ import requests
 import datetime
 import re
 import argparse
+from bs4 import BeautifulSoup
+
+def getTractInfo(url, regex=''):
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    return [url + '/' + node.get('href') for node in soup.find_all('a', href=re.compile(regex))]
+
 
 
 BASE_URL = "http://www2.census.gov/geo/docs/maps-data/data/gazetteer/"
@@ -90,7 +97,7 @@ DATA_TABLES = ['B01001','B03002','B06008','B23001','B19001','B25009','B25077']
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--states", help="State Abbreviation List, space seperated ie NY AK", nargs="*")
-parser.add_argument("-t", "--type", help="ALL|County|Upper|Lower|Congress|City|State space separated", nargs="*")
+parser.add_argument("-t", "--type", help="ALL|County|Upper|Lower|Congress|City|State|Tract space separated", nargs="*")
 
 
 def get_combinedData(thePD=None, tables=None):
@@ -163,33 +170,39 @@ def parse_voter_file_id(row):
 def get_census_data(geo_type, geo_url, state_list, fips_func,
                     state_idx=(0, 0),
                     census_tables=DATA_TABLES,
-                    find_zz=False):
-    print("Starting " + geo_type)
+                    find_zz=False,
+                    delim='\t',
+                    chunk_size=250):
+    print("Working " + geo_type)
 
     if requests.get(geo_url).status_code != 200:
         raise ValueError("{} file not found at URL: {}".format(geo_type, geo_url))
 
     # Changing source if city URL
-    if geo_type != 'City':
+    if geo_type != 'City' and geo_type != "Tract":
         csv_file = get_zip(geo_url)
         file_source = io.StringIO(csv_file.decode('cp1252'))
     else:
         file_source = geo_url
 
     reader = pd.read_csv(file_source,
-                         delimiter='\t',
+                         delimiter=delim,
                          iterator=True,
-                         chunksize=250)
+                         chunksize=chunk_size)
     context_df_list = []
     census_df_list = []
 
     for chunk in reader:
-        chunk = chunk.loc[chunk['USPS'].isin(state_list)]
+        if geo_type == "Tract":
+            geoid = 'CODE'
+        else:
+            geoid = 'GEOID'
+            chunk = chunk.loc[chunk['USPS'].isin(state_list)]
         if find_zz:
-            chunk['GEOID'] = chunk['GEOID'].astype(str)
-            chunk = chunk.loc[chunk['GEOID'].str.find('ZZ') == -1]
+            chunk[geoid] = chunk[geoid].astype(str)
+            chunk = chunk.loc[chunk[geoid].str.find('ZZ') == -1]
         if len(chunk) > 0:
-            chunk['FIPS'] = chunk['GEOID'].apply(fips_func)
+            chunk['FIPS'] = chunk[geoid].apply(fips_func)
             context_df_list.append(chunk)
             chunk = chunk.set_index('FIPS')
             data = get_combinedData(chunk, tables=census_tables)
@@ -198,7 +211,7 @@ def get_census_data(geo_type, geo_url, state_list, fips_func,
     context_df = pd.concat(context_df_list)
     census_df = pd.concat(census_df_list)
 
-    context_df['STATEFP'] = context_df['GEOID'].apply(
+    context_df['STATEFP'] = context_df[geoid].apply(
         lambda x: str(x)[:state_idx[0]].zfill(state_idx[1])
     )
     context_df['ENTITYTYPE'] = geo_type.lower()
@@ -265,6 +278,7 @@ if __name__ == '__main__':
     while requests.get(GAZ_YEAR_URL).status_code != 200:
         YEAR -= 1
         GAZ_YEAR_URL = '{}{}_Gazetteer/'.format(BASE_URL, YEAR)
+        print(GAZ_YEAR_URL)
 
     FILE_BASE_URL = GAZ_YEAR_URL + str(YEAR) + "_Gaz_"
     output_df = pd.DataFrame()
@@ -375,5 +389,35 @@ if __name__ == '__main__':
         state_df = get_state(state_list, STATE_CODES)
         state_df['NAME'] = state_df['USPS'].apply(lambda x: STATE_ABBREVS[x])
         output_df = pd.concat([output_df, state_df])
+
+    if types == 'ALL' or "TRACT" in types:
+        tracts_df_list = []
+        div_tract_df_list = []
+        temp_tract_df_list = []
+        for state in state_list:
+            tract_url = 'http://www2.census.gov/geo/maps/dc10map/tract/st{}_{}'.format(STATE_CODES[state], state.lower())
+            if state == 'PR':
+                tract_url = tract_url + "_1" #PR Just had to be different
+            print(tract_url)
+            
+            for division in getTractInfo(tract_url, "^[^#\.]+_"):
+                for tract_file in getTractInfo(division[:-1], "\.txt$"): #Just in case there are more than one
+                    print(tract_file)
+                    temp_tract_df = get_census_data(
+                       'Tract',
+                       tract_file,
+                       [state],
+                       lambda x: "14000US" + str(x).zfill(11),
+                       state_idx=(-5, 2),
+                       delim=';',
+                       chunk_size=200
+                    )
+                    temp_tract_df_list.append(temp_tract_df)
+                div_tract_df_list.append(temp_tract_df_list)
+            tracts_df_list.append(div_tract_df_list)
+        #print(tracts_df_list)
+        tract_df = pd.DataFrame(tracts_df_list)
+        output_df = pd.concat([output_df, tract_df])
+        
 
     output_df.to_csv(os.path.join(OUTPUT_DIR, "census.csv"), index_label="FIPS")
