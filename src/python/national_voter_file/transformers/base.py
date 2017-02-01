df2c34f857d885521567091d02ffe481f903ae5c
Ben <ben@peartreestudio.net>
2017-02-01 16:13:42 -0600

    Made registration status optional

--- src/python/national_voter_file/transformers/base.py
+++ src/python/national_voter_file/transformers/base.py
@@ -189,7 +189,7 @@ class BaseTransformer(object):
         'STATE_VOTER_REF': set([str]),
         'COUNTY_VOTER_REF': set([str, type(None)]),
         'REGISTRATION_DATE': set([datetime.date, type(None)]),
-        'REGISTRATION_STATUS': set([str]),
+        'REGISTRATION_STATUS': set([str, type(None)]),
         'ABSENTEE_TYPE': set([str, type(None)]),
         'PARTY': set([str, type(None)]),
         'CONGRESSIONAL_DIST': set([str]),
